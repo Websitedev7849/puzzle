7e9c875acccadfa9dc230c6f54385bf6c1ee4f4f
Siddharth Pawar <websitedev7849@gmail.com>
2021-11-01 21:29:00 +0530

    Detection of puzzle beign solved = Successfull

--- script.js
+++ script.js
@@ -1,76 +1,105 @@
-function main() {
-  const shuffle = (copyOfPics) => {
-    let myArr = copyOfPics.slice().sort( () => Math.random() - 0.5 );
-    return myArr;
+const shuffle = (copyOfPics) => {
+  let myArr = copyOfPics.slice().sort( () => Math.random() - 0.5 );
+  return myArr;
 };
 
-var pics = Array.from(document.getElementsByClassName('pic'));
+const pics = Array.from(document.querySelectorAll('.pic'));
 const copyOfPics = pics.slice();
 const shuffled = Array.from(shuffle(copyOfPics));
+let pickedElementParent;
 
-var images = document.querySelectorAll("img");
-images = Array.from(images);
-
-var pickedElementParent;
-var cursor = document.querySelector('.pointer');
-
+const cursor = document.querySelector('.pointer');
 cursor.style.top = "27%";
 
-shuffled.forEach((element) => {
-    element.style.position = "absolute";
-});
+/**
+ * Todo: figure out a way to detect finished puzzle
+ */
+
+function main() {
+
+    shuffled.forEach((element) => {
+        element.style.position = "absolute";
+    });
+
+    shuffled[0].style.top = shuffled[1].style.top = shuffled[2].style.top = "0%";
+    shuffled[3].style.top = shuffled[4].style.top = shuffled[5].style.top = "25%";
+    shuffled[6].style.top = shuffled[7].style.top = shuffled[8].style.top = "50%";
+    shuffled[9].style.top = shuffled[10].style.top = shuffled[11].style.top = "75%";
 
 
-shuffled[0].style.top = shuffled[1].style.top = shuffled[2].style.top = "0%";
-shuffled[3].style.top = shuffled[4].style.top = shuffled[5].style.top = "25%";
-shuffled[6].style.top = shuffled[7].style.top = shuffled[8].style.top = "50%";
-shuffled[9].style.top = shuffled[10].style.top = shuffled[11].style.top = "75%";
+    shuffled[0].style.left = shuffled[3].style.left = shuffled[6].style.left = shuffled[9].style.left = "0%";
+    shuffled[1].style.left = shuffled[4].style.left = shuffled[7].style.left = shuffled[10].style.left = "33%";
+    shuffled[2].style.left = shuffled[5].style.left = shuffled[8].style.left = shuffled[11].style.left = "66%";
 
 
-shuffled[0].style.left = shuffled[3].style.left = shuffled[6].style.left = shuffled[9].style.left = "0%";
-shuffled[1].style.left = shuffled[4].style.left = shuffled[7].style.left = shuffled[10].style.left = "33%";
-shuffled[2].style.left = shuffled[5].style.left = shuffled[8].style.left = shuffled[11].style.left = "66%";
-
-
-images.forEach(image => {
-    
-  image.addEventListener('click' , handleClickAndTouch);
-
-});
-
-function handleClickAndTouch(event) {
-    let holdPickedElement;
-    let left , top;
-    if (pickedElementParent == undefined) {
-        pickedElementParent = event.target.parentElement;
-        pickedElementParent.style.transform = "translate(-5%, -5%)";
-        pickedElementParent.style.zIndex = 2;
-        pickedElementParent.style.boxShadow = "0px 0px 50px rgba(0, 0, 0, 0.63)";
+    shuffled.forEach(elem => {
         
-    } else if(pickedElementParent.style.zIndex == 2){
-        
-        holdPickedElement = pickedElementParent;
-        holdPickedElement.style.transform = "";
-        holdPickedElement.style.zIndex = 0;
-        holdPickedElement.style.boxShadow = "";
-        top = holdPickedElement.style.top;
-        left = holdPickedElement.style.left;
+      elem.addEventListener('click' , handleClickAndTouch);
 
+    });
 
-        pickedElementParent = event.target.parentElement;
+    setTimeout(pointer , 1300);
 
-        holdPickedElement.style.top = pickedElementParent.style.top;
-        holdPickedElement.style.left = pickedElementParent.style.left;
-
-        pickedElementParent.style.top = top;
-        pickedElementParent.style.left = left;
-       
-        pickedElementParent = undefined;
-    }
-   
 }
 
-var pointer = function(){
+function verify(pics) {
+
+  const top0 = (pics[0].style.top == '0%' && pics[1].style.top == '0%' && pics[2].style.top == '0%');
+  const top25 = (pics[3].style.top == '25%' && pics[4].style.top == '25%' && pics[5].style.top == '25%');
+  const top50 = (pics[6].style.top == '50%' && pics[7].style.top == '50%' && pics[8].style.top == '50%');
+  const top75 = (pics[9].style.top == '75%' && pics[10].style.top == '75%' && pics[11].style.top == '75%');
+
+  const left0 = (pics[0].style.left == '0%' && pics[3].style.left == '0%' && pics[6].style.left == '0%' && pics[9].style.left == '0%');
+  const left33 = (pics[1].style.left == '33%' && pics[4].style.left == '33%' && pics[7].style.left == '33%' && pics[10].style.left == '33%');
+  const left66 = (pics[2].style.left == '66%' && pics[5].style.left == '66%' && pics[8].style.left == '66%' && pics[11].style.left == '66%');
+
+  // console.log(top0 ,top25 ,top50 ,top75 , left0, left33, left66);
+
+  if (top0 && top25 && top50 && top75 && left0 && left33 && left66) 
+    return true;
+  
+  return false;
+  
+}
+
+function handleClickAndTouch(event) {
+  let holdPickedElement;
+  let left , top;
+  if (pickedElementParent == undefined) { // 0 pic selected
+      pickedElementParent = event.target.parentElement;
+      pickedElementParent.style.transform = "translate(-5%, -5%)";
+      pickedElementParent.style.zIndex = 2;
+      pickedElementParent.style.boxShadow = "0px 0px 50px rgba(0, 0, 0, 0.63)";
+      
+  } else if(pickedElementParent.style.zIndex == 2){ // 1 pic selected to be replaced
+      
+      holdPickedElement = pickedElementParent;
+      holdPickedElement.style.transform = "";
+      holdPickedElement.style.zIndex = 0;
+      holdPickedElement.style.boxShadow = "";
+      top = holdPickedElement.style.top;
+      left = holdPickedElement.style.left;
+
+
+      pickedElementParent = event.target.parentElement;
+
+      holdPickedElement.style.top = pickedElementParent.style.top;
+      holdPickedElement.style.left = pickedElementParent.style.left;
+
+      pickedElementParent.style.top = top;
+      pickedElementParent.style.left = left;
+    
+      pickedElementParent = undefined;
+  }
+
+  if (verify(pics)) {
+    console.log("puzzle solved");
+  }
+ 
+
+}
+
+function pointer(){
   cursor.style.display = "block";
   let initialElement , finalElement;
   
@@ -115,12 +144,6 @@ var pointer = function(){
   }, 3000);
 
 }
-
-
-setTimeout(pointer , 1300);
-
-}
-
 
 
 window.addEventListener('load' , function () {
